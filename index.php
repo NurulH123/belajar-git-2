530ede73eb5ebd47ff72a76bb48c046054822f8e
NurulH123 <hidayatnurul937@gmail.com>
2020-06-25 14:17:05 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Git Remote</title>
+</head>
+<body>
+    <h2>Menambah List</h2>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+</body>
+</html>
